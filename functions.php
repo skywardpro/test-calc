1966604316037d3ee838dc3a86a513a7d72288c4
Nick Plotnikov <mail@skyward.pro>
2020-11-16 14:05:30 +0300

    feat: add react action

--- functions.php
+++ functions.php
@@ -1,12 +1,22 @@
+<?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-    $theme = wp_get_theme();
-    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
-        array(),  // if the parent theme code has a dependency, copy it to here
-        $theme->parent()->get('Version')
-    );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( $parenthandle ),
-        $theme->get('Version') // this only works if you have Version in the style header
-    );
+ 
+  $parent_style = 'twentynineteen-style'; 
+ 
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+ 
+  wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    [ $parent_style ],
+    time() //wp_get_theme()->get('Version')
+  );
+ 
+  wp_enqueue_script(
+    'my-theme-frontend',
+    get_stylesheet_directory_uri() . '/build/index.js',
+    ['wp-element'],
+    time() //For production use wp_get_theme()->get('Version')        
+  );
+ 
+}
